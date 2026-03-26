b700c5e6b16d80a45e99ed76d0312f06a1c05ba5
abiyyurafaramadhan2-tech <abiyyurafaramadhan2@gmail.com>
2026-03-26 15:20:44 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Action RPG</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
